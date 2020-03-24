a55dd16f1c32efb2c763f915cc2a24956246ef46
gmanriquev <gmanrique.422@gmail.com>
2020-03-23 22:04:10 -0500

    Semana 03.03

--- SEM-03/Parte03.sql
+++ SEM-03/Parte03.sql
@@ -44,3 +44,37 @@ from Trabajador t
 inner join Persona p on t.idpersona=p.idpersona
 left join Asignacion a on t.idtrabajador=a.idencuestador
 where t.estado=1 and t.tipo='E'
+
+--3.9
+select 
+usuario,
+p.nombres+''+p.apellidos as NOMBRE_COMPLETO,
+ISNULL(m.nombre,'0000') as MANZANA,
+ISNULL(a.fecinicio,'9999-12-31') as FEC_INICIO,
+ISNULL(a.fecfin,'9999-12-31') as FEC_FIN
+from Trabajador t --TI (PREVALECE)
+left join Asignacion a on t.idtrabajador=a.idencuestador --TD
+--NOMBRE_COMPLETO
+left join Persona p on p.idpersona=t.idpersona
+--MANZANA
+left join Manzana m on m.idmanzana=a.idmanzana
+where t.estado=1 and t.tipo='E'
+order by usuario desc
+
+select * from Trabajador t
+where t.estado=1 and t.tipo='E'
+
+--3.10
+--Trabajador (TI)
+--Asignacion (TD) (PREVALECE)
+
+select
+isnull(t.usuario,'-') as USUARIO,
+case when t.idpersona is null then '-' else concat('P',t.idpersona) end as ID_PERSONA,
+--t.idtrabajador,
+a.idencuestador,
+a.idmanzana,
+ISNULL(a.fecinicio,'9999-12-31') as FEC_INICIO,
+ISNULL(a.fecfin,'9999-12-31') as FEC_FIN
+from Trabajador t
+right join Asignacion a on t.idtrabajador=a.idencuestador
