6231efa152658b04ad3236b53e32a118bceda1e0
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-04-13 20:20:49 -0500

    Archivos Semana 06

--- SEM-06/Parte01.sql
+++ SEM-06/Parte01.sql
@@ -33,3 +33,51 @@ inner join TipoDocumento t on p.idtipo=t.idtipo
 --Consulta a VISTA
 select * from V_PERSONA_C
 order by idtipo,numdoc,nombres,apellidos
+
+--B
+create view V_PERSONA_IR
+as
+WITH CTE_PERSONA_C AS
+(
+	select idtipo,numdoc,nombres,apellidos from Persona --997
+	union  --combinaciones irrepetibles
+	select idtipo,numdoc,nombres,apellidos from PersonaCarga --505
+)
+select t.nombre,p.idtipo,p.numdoc,p.nombres,p.apellidos from CTE_PERSONA_C p
+inner join TipoDocumento t on p.idtipo=t.idtipo
+
+select * from V_PERSONA_IR --930
+order by idtipo,numdoc,nombres,apellidos
+
+--C
+--create function F_PERSONA_DETALLE
+alter function F_PERSONA_DETALLE
+(@idtipo int,@numdoc varchar(15)) returns table
+as
+return
+	WITH CTE_PERSONA_C AS
+	(
+		select idtipo,numdoc,nombres,apellidos from Persona --997
+		union  all
+		select idtipo,numdoc,nombres,apellidos from PersonaCarga --505
+	)
+	select t.nombre,p.idtipo,p.numdoc,p.nombres,p.apellidos from CTE_PERSONA_C p
+	inner join TipoDocumento t on p.idtipo=t.idtipo
+	where p.idtipo=@idtipo and p.numdoc=@numdoc
+
+	select * from F_PERSONA_DETALLE(1,'00488191')
+
+--06.02
+--A
+create view CTE_UBIGEO_C 
+as
+WITH CTE_UBIGEO_C AS
+(
+	select departamento,provincia,distrito from UbigeoCarga --1664
+	union  --combinaciones irrepetibles
+	select nom_dpto,nom_prov,nom_dto from Ubigeo --12
+)
+select departamento,provincia,distrito from CTE_UBIGEO_C 
+
+select * from CTE_UBIGEO_C
+order by departamento,provincia,distrito
